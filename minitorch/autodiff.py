477b4d7642f253a7e66d02de43c2fc7a4c6126f0
Serebrennikov Dan <serebro.dann@gmail.com>
2026-09-23 23:11:58 +0300

    fix

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -22,8 +22,9 @@ def central_difference(f: Any, *vals: Any, arg: int = 0, epsilon: float = 1e-6) 
     Returns:
         An approximation of $f'_i(x_0, \ldots, x_{n-1})$
     """
-    # TODO: Implement for Task 1.1.
-    raise NotImplementedError('Need to implement for Task 1.1')
+    vals1 = [v + epsilon if i == arg else v for i, v in enumerate(vals)]
+    vals2 = [v - epsilon if i == arg else v for i, v in enumerate(vals)]
+    return (f(*vals1) - f(*vals2)) / (2 * epsilon)
 
 
 variable_count = 1
@@ -61,8 +62,21 @@ def topological_sort(variable: Variable) -> Iterable[Variable]:
     Returns:
         Non-constant Variables in topological order starting from the right.
     """
-    # TODO: Implement for Task 1.4.
-    raise NotImplementedError('Need to implement for Task 1.4')
+    order: List[Variable] = []
+    seen = set()
+
+    def visit(var: Variable) -> None:
+        if var.unique_id in seen or var.is_constant():
+            return
+        if not var.is_leaf():
+            for parent in var.parents:
+                if not parent.is_constant():
+                    visit(parent)
+        seen.add(var.unique_id)
+        order.insert(0, var)
+
+    visit(variable)
+    return order
 
 
 def backpropagate(variable: Variable, deriv: Any) -> None:
@@ -76,8 +90,20 @@ def backpropagate(variable: Variable, deriv: Any) -> None:
 
     No return. Should write to its results to the derivative values of each leaf through `accumulate_derivative`.
     """
-    # TODO: Implement for Task 1.4.
-    raise NotImplementedError('Need to implement for Task 1.4')
+    order = topological_sort(variable)
+    derivatives = {variable.unique_id: deriv}
+
+    for var in order:
+        d_output = derivatives[var.unique_id]
+        if var.is_leaf():
+            var.accumulate_derivative(d_output)
+        else:
+            for parent, d in var.chain_rule(d_output):
+                if parent.is_constant():
+                    continue
+                derivatives[parent.unique_id] = (
+                    derivatives.get(parent.unique_id, 0.0) + d
+                )
 
 
 @dataclass
